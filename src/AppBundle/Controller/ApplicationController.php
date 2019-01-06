5faf605913807499fd60bb29b2d872d483a9ab5d
Ruben Hollevoet <ruben.hollevoet@intracto.com>
2019-01-06 20:10:29 +0100

    major update onkostentool

--- src/AppBundle/Controller/ApplicationController.php
+++ src/AppBundle/Controller/ApplicationController.php
@@ -20,9 +20,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Validator\Tests\Fixtures\ToString;
 
 class ApplicationController extends Controller
 {
@@ -420,6 +426,185 @@ class ApplicationController extends Controller
         return $this->json([
             'status' => 'ok',
         ]);
+    }
+
+    /**
+     * @Route("/api/getRegionsForExport")
+     */
+    public function getRegionsForExport() {
+
+        $em = $this->getDoctrine()->getManager();
+        $regions = $em->getRepository(Region::class)->findAll();
+        $trips = $em->getRepository(Trip::class)->findBy(['status' => 'approved']);
+
+        $data = [];
+
+        foreach ($regions as $region) {
+            $data[$region->getId()] = [
+                'id' => $region->getId(),
+                'data' => [
+                    'id' => $region->getId(),
+                    'name' => $region->getName(),
+                    'start' => null,
+                    'end' => null,
+                    'price' => 0,
+                    'count' => 0
+                ]
+            ];
+        }
+
+
+        foreach ($trips as $trip) {
+            $id = $trip->getRegion()->getId();
+            $data[$id]['data']['count']++;
+            $data[$id]['data']['price'] += $trip->getPrice();
+            if($data[$id]['data']['start'] === null || $data[$id]['data']['start'] > strtotime($trip->getDate()->format('Y-m-d')) ) $data[$id]['data']['start'] = strtotime($trip->getDate()->format('Y-m-d'));
+            if($data[$id]['data']['end'] === null || $data[$id]['data']['end'] < strtotime($trip->getDate()->format('Y-m-d')) ) $data[$id]['data']['end'] = strtotime($trip->getDate()->format('Y-m-d'));
+        }
+
+        //format date time
+        foreach ($regions as $region) {
+            $data[$region->getId()]['data']['start'] = date('d-m-Y', $data[$region->getId()]['data']['start']);
+            $data[$region->getId()]['data']['end'] = date('d-m-Y', $data[$region->getId()]['data']['end']);
+
+
+            $exports = [];
+            $exportDir = $this->getParameter('export_path').'/'.$region->getId().'/';
+            if(file_exists($exportDir)) {
+                $exports = scandir($exportDir, 1);
+            }
+
+            //remove . and .. form export arr
+            $filteredExports = [];
+            foreach($exports as $export)
+            {
+                if(is_file($exportDir.$export)) {
+                    $filteredExports[] = $export;
+                }
+            }
+
+            $data[$region->getId()]['data']['exports'] = $filteredExports;
+        }
+
+        return $this->json([
+            'status' => 'ok',
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * @Route("/admin/generateExportFile/{regionId}/{type}", name="exportExpenses")
+     */
+    public function generateExportFile(Request $request, $regionId, $type) {
+
+        $em = $this->getDoctrine()->getManager();
+        $region = $em->getRepository(Region::class)->find($regionId);
+
+        if($region === null) {
+            throw new NotFoundHttpException('Unable to find region. Region is equal to null');
+        }
+
+        $trips = $em->getRepository(Trip::class)->findBy(['status' => 'approved', 'region' => $region]);
+
+        $tripGroups = [];
+
+        foreach ($trips as $trip) {
+
+            $activityStack = [$trip->getGroup()->getName()];
+            if($trip->getGroup()->getParent()) {
+                array_unshift($activityStack, $trip->getGroup()->getParent()->getName());
+                if($trip->getGroup()->getParent()->getParent()) {
+                    array_unshift($activityStack, $trip->getGroup()->getParent()->getParent()->getName());
+                }
+            }
+
+
+            $groupCode = $trip->getGroup()->getCode();
+            if(!array_key_exists($groupCode, $tripGroups)) {
+                $tripGroups[$groupCode] = [
+                    'code' => $trip->getGroup()->getCode(),
+                    'group' => $trip->getGroup()->getCode() ? implode(' > ', $activityStack) : 'onbekend',
+                    'users' => [],
+                ];
+            }
+
+            $userId = $trip->getUser()->getId();
+            if(!array_key_exists($userId, $tripGroups[$groupCode]['users'])) {
+                $tripGroups[$groupCode]['users'][$userId] = [
+                    'price' => 0,
+                    'name' => $trip->getUser()->getFirstName().' '.$trip->getUser()->getLastName(),
+                    'personId' => $trip->getUser()->getPersonId(),
+                    'iban' => $trip->getUser()->getIban(),
+                    'address' => $trip->getUser()->getAddress(),
+                    'trips' => []
+                ];
+            }
+
+            $tripGroups[$groupCode]['users'][$userId]['price'] += $trip->getPrice();
+            $tripGroups[$groupCode]['users'][$userId]['trips'][] = [
+                'group' => implode(' > ', $activityStack),
+                'activity' => $trip->getActivity(),
+                'comment' => $trip->getComment(),
+                'adminComment' => $trip->getCommentAdmin(),
+                'distance' => $trip->getDistance(),
+                'price' => $trip->getPrice(),
+                'from' => $trip->getFrom(),
+                'to' => $trip->getTo()
+            ];
+
+//            if($type === 'final') {
+//                $trip->setStatus('processed');
+//                $em->persist($trip);
+//            }
+        }
+
+        ksort($tripGroups);
+
+        $export = $this->render('exportTemplates/expensesExport.html.twig', [
+            'trips' => $tripGroups,
+            'isFinal' => $type === 'final'
+        ]);
+
+        if($type === 'final') {
+//            $em->flush();
+
+            $fileName = 'export_'.date("i-s-j-n-Y").'_'.count($trips).'_'.$this->getUser()->getFirstName().'.html';
+            $dir = $this->getParameter('export_path').'/'.$region->getId();
+            if (!file_exists($dir)) {
+                mkdir($dir, 0777, true);
+            }
+
+            file_put_contents(
+                $dir.'/'.$fileName,
+                $export
+            );
+
+            $this->addFlash('success', 'Een nieuwe export is toegevoegd aan de finale export lijst.');
+
+            return $this->redirect('/admin?action=list&entity=TripExport');
+        }
+        else {
+            return $export;
+        }
+    }
+
+    /**
+     * @Route("/admin/downloadExport/{regionId}/{fileName}", name="downloadExport")
+     *
+     * @var string $fileName
+     * @var string $regionId
+     *
+     * @return BinaryFileResponse
+     */
+    public function downloadExportFile($fileName, $regionId) {
+        $dir = $this->getParameter('export_path').'/'.$regionId;
+
+        $response = new BinaryFileResponse($dir.'/'.$fileName);
+
+        $response->headers->set('Content-Type', 'text/html');
+        $response->headers->set('Content-Disposition', 'attachment; filename="'.$fileName.'"');
+
+        return $response;
     }
 
     /**
